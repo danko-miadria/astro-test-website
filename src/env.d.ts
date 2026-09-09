caeedbc12def53ddf070c2a9a80344193c549961
dankogrg <dankogrg@gmail.com>
2026-09-09 11:38:52 +0200

    new blog

--- src/env.d.ts
+++
@@ -1,8 +0,0 @@
-interface NetlifyIdentity {
-    on(event: "init", callback: (user: unknown) => void): void;
-    on(event: "login", callback: () => void): void;
-}
-
-interface Window {
-    netlifyIdentity?: NetlifyIdentity;
-}
