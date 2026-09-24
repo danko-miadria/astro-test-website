4b7cd6c44d3dc86e415e8d5fbf6489210dd15e63
dankogrg <dankogrg@gmail.com>
2026-09-24 13:58:21 +0200

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Your GitHub Pages site URL:
+    site: "https://danko-miadria.github.io",
+    // The repository path (only required if NOT using a custom domain or user page):
+    base: "/astro-test-website",
+});
